7f188454a63280e54de15fff255f090d224f8280
Сергей Совостьянов <ssovostyanov@mail.ru>
2025-03-26 22:39:05 +0300

    fix Auth, fix bug firebase

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,39 +1,44 @@
 import {FC, FormEvent, useState} from 'react';
-import { auth, signInWithEmailAndPassword, sendPasswordResetEmail} from '../../firebase.ts';
+import {
+    auth,
+    signInWithEmailAndPassword,
+    sendPasswordResetEmail,
+    createUserWithEmailAndPassword
+} from '../../firebase.ts';
 import './Auth.scss';
 
 const Auth: FC = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [isLogin, setIsLogin] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>('');
+    const [error, setError] = useState<string | null>(null);
 
-    const handleAuth= async (e: FormEvent) => {
+    const handleAuth = async (e: FormEvent) => {
         e.preventDefault();
         setError(null);
         try {
             if (isLogin) {
                 await signInWithEmailAndPassword(auth, email, password);
-                alert('Авторизация прошла успешно')
+                alert('Авторизация прошла успешно');
             } else {
-                createUserWithEmailAndPassword(auth, email, password);
-                alert('Успешная регистрация')
+                await createUserWithEmailAndPassword(auth, email, password);
+                alert('Успешная регистрация');
             }
-        } catch(error) {
-            setError(error.message)
+        } catch(error: any) {
+            setError(error.message);
         }
     }
 
     const handleForgotPassword = async () => {
         if (!email) {
-            setEmail("Введите Email для восстановления пароля")
-            return
+            setError("Введите Email для восстановления пароля");
+            return;
         }
 
         try {
             await sendPasswordResetEmail(auth, email);
             alert('Письмо для восстановления отправлено на почту');
-        } catch(error) {
+        } catch(error: any) {
             setError(error.message);
         }
     }
@@ -41,17 +46,30 @@ const Auth: FC = () => {
     return (
         <>
             <form onSubmit={handleAuth} className="form-auth">
-                <h3>Авторизация</h3>
+                <h3>{isLogin ? 'Авторизация' : 'Регистрация'}</h3>
+                {error && <div style={{color: '#f76b04', marginBottom: '10px'}}>{error}</div>}
                 <div className="form-input">
-                    <input onChange={(e) => setEmail(e.target.value)} type="text" id="email" value={email} placeholder="Почта: "/>
-                    <input onChange={(e) => setPassword(e.target.value)} type="text" id="password" value={password} placeholder="Пароль: "/>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        id="email"
+                        value={email}
+                        placeholder="Почта:"
+                    />
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        id="password"
+                        value={password}
+                        placeholder="Пароль:"
+                    />
                 </div>
                 <button type='submit' className="auth-btn">
                     {isLogin ? 'Войти' : 'Зарегистрироваться'}
                 </button>
                 <div className="form-checkbox">
-                    <input type="checkbox" name="save-computer"/>
-                    <label htmlFor="save-computer" id="save-computer">Запомнить на этом компьютере</label>
+                    <input type="checkbox" id="save-computer"/>
+                    <label htmlFor="save-computer">Запомнить на этом компьютере</label>
                 </div>
             </form>
             <div className="flex-references">
@@ -60,10 +78,6 @@ const Auth: FC = () => {
                     {isLogin ? 'Регистрация' : 'Войти'}
                 </button>
             </div>
-            {/*<div className="flex-references">*/}
-            {/*    <Link to="/reset">Забыли пароль?</Link>*/}
-            {/*    <Link to="/registration">Регистрация</Link>*/}
-            {/*</div>*/}
         </>
     );
 };
